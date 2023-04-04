429de4a169d64a72c57f735a73722f8480cae94c
ahmermirza <ahmermirzaonline@gmail.com>
2023-04-04 21:15:47 +0500

    soft delete added for likes table

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth')->only('store', 'destroy');
+    }
+
     public function index()
     {
         $posts = Post::with(['user', 'likes'])->orderBy('created_at', 'desc')->paginate(6);
--- app/Like.php
+++ app/Like.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Like extends Model
 {
+    use SoftDeletes;
+    
     protected $fillable = ['user_id'];
 }
